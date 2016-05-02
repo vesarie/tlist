ff8d0e0a1f280bcce3e0b477e84f97e7a222c1a5
Vesa Riekkinen <vesa.riekkinen@gmail.com>
2016-05-02 21:45:28 +0300

    Removed the color-attribute

--- sql/add-test-data.sql
+++ sql/add-test-data.sql
@@ -1,19 +1,19 @@
-insert into Person (forename, surname, email, password) values ('Alex',  'Adams', 'alex.adams@example.com', '');
+insert into Person (forename, surname, email, password) values ('Test',  'User',  'test@user.com', 'pwd');
 insert into Person (forename, surname, email, password) values ('Bill',  'Baker', 'bill.baker@example.com', '');
 insert into Person (forename, surname, email, password) values ('Chloe', 'Clark', 'chloe.clark@example.com', '');
 insert into Person (forename, surname, email, password) values ('Diana', 'Davis', 'diana.davis@example.com', '');
 
-insert into Project (person, name, color) values (1, 'tsoha',    x'00ffff'::int);
-insert into Project (person, name, color) values (1, 'tira',     x'0000ff'::int);
-insert into Project (person, name, color) values (1, 'hobbies',  x'ffa500'::int);
-insert into Project (person, name, color) values (2, 'work',     x'ffff00'::int);
-insert into Project (person, name, color) values (2, 'school',   x'00ff00'::int);
-insert into Project (person, name, color) values (2, 'personal', x'aa00ff'::int);
-insert into Project (person, name, color) values (2, 'etc',      x'ff0000'::int);
-insert into Project (person, name, color) values (3, 'work',     x'ffff00'::int);
-insert into Project (person, name, color) values (3, 'school',   x'00ff00'::int);
-insert into Project (person, name, color) values (3, 'personal', x'aa00ff'::int);
-insert into Project (person, name, color) values (3, 'etc',      x'ff0000'::int);
+insert into Project (person, name) values (1, 'tsoha');
+insert into Project (person, name) values (1, 'tira');
+insert into Project (person, name) values (1, 'hobbies');
+insert into Project (person, name) values (2, 'work');
+insert into Project (person, name) values (2, 'school');
+insert into Project (person, name) values (2, 'personal');
+insert into Project (person, name) values (2, 'etc');
+insert into Project (person, name) values (3, 'work');
+insert into Project (person, name) values (3, 'school');
+insert into Project (person, name) values (3, 'personal');
+insert into Project (person, name) values (3, 'etc');
 
 insert into Task (project, priority, schedule, completed, name) values (1,  4, '2016-04-03', true,  'Write create table commands');
 insert into Task (project, priority, schedule, completed, name) values (1,  4, '2016-04-03', true,  'Create some test data (add-test-data.sql)');
@@ -55,8 +55,8 @@ insert into Task (project, priority, schedule, completed, name) values (11, 4, '
 insert into Task (project, priority, schedule, completed, name) values (11, 4, '2016-04-07', false, 'Task etc.2');
 insert into Task (project, priority, schedule, completed, name) values (11, 4, '2016-04-08', false, 'Task etc.3');
 
-insert into Label (person, name, color) values (1, 'work',   x'ff4000'::int);
-insert into Label (person, name, color) values (1, 'school', x'ffff00'::int);
-insert into Label (person, name, color) values (1, 'home',   x'40ff00'::int);
+insert into Label (person, name) values (1, 'work');
+insert into Label (person, name) values (1, 'school');
+insert into Label (person, name) values (1, 'home');
 
 insert into TaskLabel (task, label) values (3, 2);
--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -10,7 +10,6 @@ create table Project (
     id serial primary key,
     person integer not null,
     name varchar(100) not null,
-    color integer not null,
     foreign key (person) references Person(id)
 );
 
@@ -18,7 +17,6 @@ create table Label (
     id serial primary key,
     person integer not null,
     name varchar(100) not null,
-    color integer not null,
     foreign key (person) references Person(id)
 );
 
